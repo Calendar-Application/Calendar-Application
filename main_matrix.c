e1d4c9a94895d263a5e80a5d868f38e06debf478
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2018-09-16 19:21:45 +0530

    Revisions: + added year and month view (-) not yet commented: documentation left

--- main_matrix.c
+++ main_matrix.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
 
 int month_code(int month)
 {
@@ -10,10 +11,11 @@ int month_code(int month)
 
 int day_index(int date, int month, int year)
 {
+    int y=year;
     if(month==1 || month==2)
-        year--;
+        y--;
 
-    int i = (date + ((13*month_code(month)-1)/5) + (year%100) + ((year%100)/4) + ((year/100)/4) - 2*(year/100));
+    int i = (date + (((13*month_code(month))-1)/5) + (y%100) + ((y%100)/4) + ((y/100)/4) - 2*(y/100));
     int f = i % 7;
 
     if(f<0)
@@ -38,12 +40,117 @@ int no_days_in_month(int month, int year)
         return d[month-1];
 }
 
+void month_matrix(int month_m[6][7], int month, int year)
+{
+    int i;
+    for(i=0; i<6; i++)
+        for(int j=0; j<7; j++)
+            month_m[i][j]=0;
+
+    int k = day_index(1, month, year);
+    int days = no_days_in_month(month, year);
+
+    i=0;
+    int d=1;
+    while(1)
+    {
+        month_m[i][k]=d;
+
+        if(k==6)
+        {
+            i++;
+            k=0;
+        }
+        else
+            k++;
+
+        if(d==days)
+            break;
+        else
+            d++;
+    }
+}
+
+char* get_month_name(int month)
+{
+    char* m[] = {"January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"};
+
+    return m[month-1];
+}
+
+void print_month(int matrix[6][7], int month)
+{
+    int space = (int)((27 - strlen(get_month_name(month)))/2);
+    for(int i=0; i<space; i++)
+        printf(" ");
+
+    printf("%s\n", get_month_name(month));
+    printf("Sun Mon Tue Wed Thu Fri Sat\n");
+
+    for(int i=0; i<6; i++)
+    {
+        for(int j=0; j<7; j++)
+        {
+            if(matrix[i][j]==0)
+                printf("   ");
+            else
+                printf("%3d", matrix[i][j]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+void print_year(int year)
+{
+    int month_m[6][7];
+
+    int dig = floor(log10(year))+1;
+    int space = (int)((27 - dig)/2);
+    for(int i=0; i<space; i++)
+        printf(" ");
+
+    printf("%d\n\n", year);
+
+    for(int i=0; i<12; i++)
+    {
+        month_matrix(month_m, i+1, year);
+        print_month(month_m, i+1);
+        printf("\n");
+    }
+}
+
 int main()
 {
-    int date, month, year;
+    int choice, month, year, month_m[6][7];
 
-    scanf("%d%d%d", &date, &month, &year);
-    printf("%d", no_days_in_month(month, year));
+    printf("Enter 1 - Year view || 2 - Month View: ");
+    scanf("%d", &choice);
 
+    switch (choice)
+    {
+        case 1:
+            printf("Enter Year: ");
+            scanf("%d", &year);
+
+            print_year(year);
+
+            break;
+        case 2:
+            printf("Enter Month and Year: ");
+            scanf("%d%d", &month, &year);
+
+            month_matrix(month_m, month, year);
+            print_month(month_m, month);
+
+            break;
+
+        default:
+            printf("Wrong choice");
+            break;
+    }
+    printf("\n\n");
     return 0;
+
 }
